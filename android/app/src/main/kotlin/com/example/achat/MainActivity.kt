fe68daf565a7bd8bdd272173833d6f91dc975a33
unknown <platonn250@gmail.com>
2022-08-11 12:48:34 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/achat/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.achat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+// ignore_for_file: prefer_const_constructors, duplicate_import
+
+import 'package:achat/screens/welcome_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:achat/screens/welcome_screen.dart';
+import 'package:achat/screens/login_screen.dart';
+import 'package:achat/screens/registration_screen.dart';
+import 'package:achat/screens/chat_screen.dart';
+
+void main() => runApp(FlashChat());
+
+class FlashChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        textTheme: TextTheme(
+          headline1: TextStyle(color: Colors.black54),
+        ),
+      ),
+      home: WelcomeScreen(),
+    );
+  }
+}
